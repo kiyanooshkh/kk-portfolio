d2ce5782d2462f8a71ef81c2d9a0b47aef1fee02
kiyanooshkh <khosravi.kiyanoosh@gmail.com>
2018-08-03 18:57:51 +0930

    update styling menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = {
+  fontColor: {
+    color:"#fff",
+  },
+};
 
-class Menu extends React.Component { 
-  render() {
+function Menu (props) { 
+  const { classes } = props;
       return (
         <div>
-        <Button>Portfolio</Button>
-        <Button>About</Button>
-        <Button>Phototgraphy</Button>
-        <Button>Contact</Button>
-      </div>
-      )
-
-  }
+          <Button className={classes.fontColor}>Portfolio</Button>
+          <Button className={classes.fontColor}>About</Button>
+          <Button className={classes.fontColor}>Phototgraphy</Button>
+          <Button className={classes.fontColor}>Contact</Button>
+        </div>
+      );
 }
 
-export default (Menu);
+export default withStyles(styles)(Menu);
 
   
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,71 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Menu from './menu';
+import '../css/tools.css';
 
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
   flex: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
   },
 };
 
 function Navigation(props) {
   const { classes } = props;
   return (
-    <div className={classes.root}>
       <AppBar position="static">
-      <div container spacing={24}>
-      <Toolbar>
+      <Toolbar className="container">
           <Typography variant="title" color="inherit" className={classes.flex}>
             Logo
           </Typography>
           <Menu/>
         </Toolbar>
-      </div>
       </AppBar>
-
-        <Grid container={true} spacing={24}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
-    
   );
 }
-
-Navigation.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(Navigation);
