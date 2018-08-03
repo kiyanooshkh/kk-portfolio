3e4234041d80af6d2f247ac31124ffe78a020b1f
kiyanooshkh <khosravi.kiyanoosh@gmail.com>
2018-08-03 22:14:07 +0930

    blue love

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Navigation from './components/nav'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 
 class App extends Component {
   render() {
-    const theme = createMuiTheme();
+    const theme = createMuiTheme(
+
+      {
+        palette: {
+          primary: blue,
+          secondary: blue,
+        },
+      }
+    );
+    
     return (
       <div>
        <MuiThemeProvider theme={theme}>
