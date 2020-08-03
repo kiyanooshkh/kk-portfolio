4b16ab0072a857b443728e364acc20f429479c10
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-03 23:51:49 +0000

    Deploy kiyanooshkh/react-portfolio-example to github.com/kiyanooshkh/react-portfolio-example.git:gh-pages

---
+++ precache-manifest.61becd9b5eeeaf7532bc1f62d13e1206.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bf574589643c99c014007cddae1531",
+    "url": "/react-portfolio-example/index.html"
+  },
+  {
+    "revision": "038d3ccc06acfd0c0feb",
+    "url": "/react-portfolio-example/static/css/main.2a6b5459.chunk.css"
+  },
+  {
+    "revision": "f7703b81b3c041d2491c",
+    "url": "/react-portfolio-example/static/js/2.ebe2ecb9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio-example/static/js/2.ebe2ecb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038d3ccc06acfd0c0feb",
+    "url": "/react-portfolio-example/static/js/main.a70f6826.chunk.js"
+  },
+  {
+    "revision": "a2123ff2aa6c07e06e6e",
+    "url": "/react-portfolio-example/static/js/runtime-main.efceba24.js"
+  },
+  {
+    "revision": "0df0a96bfd3fae765d458c491fd28dce",
+    "url": "/react-portfolio-example/static/media/kiyanoosh-logo.0df0a96b.svg"
+  },
+  {
+    "revision": "92d457f89fb03023005618aec2f13c92",
+    "url": "/react-portfolio-example/static/media/kiyanoosh-profile.92d457f8.jpg"
+  }
+]);
